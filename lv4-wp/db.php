a3e622daf4131e4423f4604c3503348e3276402d
KTamara <tamara.kaselj00@gmail.com>
2026-05-25 11:31:57 +0200

    Delete lv4-wp directory

--- lv4-wp/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-$con = mysqli_connect("localhost", "root", "", "glazba_db");
-
-if (mysqli_connect_errno()) {
-    echo "Neuspješno povezivanje s MySQL-om: " . mysqli_connect_error();
-    die();
-}
-?>
